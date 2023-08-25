106a3e625bb9d3522c21acdef99182f716f95986
idlesseby <sebastian.moedritzer@gmail.com>
2023-08-25 21:21:18 +0200

    Make chords dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import React from 'react'
 import './App.css'
+import chords from './chords.js'
+import titles from './titles.js'
 import Notation from './Notation'
 
 const App = () => {
+
+  const [title, setTitle] = React.useState('Memories')
+
+  const getTitle = () => {
+    setTitle(() => {
+      return titles[Math.floor(Math.random() * 299)]
+    })
+  }
+
   return <>
     <header>
       <p>logo</p>
@@ -9,10 +21,14 @@ const App = () => {
     </header>
     <main>
       <section>
-        <div className="progression-name">Memories</div>
-        <Notation/>
-        <Notation/>
-        <button className="progression-button" type="button">Generate Chords</button>
+        <div className="progression-name">{title}</div>
+        <div className="container">
+          <Notation chord={chords[0]}/>
+          <Notation chord={chords[1]}/>
+          <Notation chord={chords[0]}/>
+          <Notation chord={chords[1]}/>
+        </div>
+        <button className="progression-button" type="button" onClick={getTitle}>Generate Chords</button>
       </section>
     </main>
   </>
--- src/Notation.jsx
+++ src/Notation.jsx
@@ -1,67 +1,48 @@
 import './Notation.css'
 
-const Notation = () => {
+const Notation = (chord) => {
+
   return (
     <table className='chord'>
-      <caption>Am</caption>
+      <caption>{chord.chord.name}</caption>
       <thead>
         <tr>
           <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th>
-            <div className='muted'>x</div>
-          </th>
-          <th></th>
+          {chord.chord.mutedStrings.map((muted, index) => {
+            return <th key={index}>{muted === 1 ? (<div className='muted'>x</div>) : null}</th>
+          })}
         </tr>
       </thead>
       <tbody>
         <tr>
-          <th>1</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <th>{chord.chord.frets[0]}</th>
+          {chord.chord.tab.map((note, index) => {
+            return <td key={index}>{note === 1 ? (<div className='circle'></div>) : null}</td>
+          })}
         </tr>
         <tr>
           <th></th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          {chord.chord.tab.map((note, index) => {
+            return <td key={index}>{note === 2 ? (<div className='circle'></div>) : null}</td>
+          })}
         </tr>
         <tr>
-          <th>3</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <th>{chord.chord.frets[1]}</th>
+          {chord.chord.tab.map((note, index) => {
+            return <td key={index}>{note === 3 ? (<div className='circle'></div>) : null}</td>
+          })}
         </tr>
         <tr>
           <th></th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          {chord.chord.tab.map((note, index) => {
+            return <td key={index}>{note === 4 ? (<div className='circle'></div>) : null}</td>
+          })}
         </tr>
         <tr>
-          <th>5</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <th>{chord.chord.frets[2]}</th>
+          {chord.chord.tab.map((note, index) => {
+            return <td key={index}>{note === 5 ? (<div className='circle'></div>) : null}</td>
+          })}
         </tr>
       </tbody>
     </table>
